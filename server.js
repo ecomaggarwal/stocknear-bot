9743cb820f4158962e801bc0eaa63c6990864841
Sainyam <80028778+SainyamAgg@users.noreply.github.com>
2026-06-10 16:18:26 +0530

    Remove test reminder route

--- server.js
+++ server.js
@@ -133,11 +133,6 @@ app.get("/send-test", async (req, res) => {
   res.send("Message sent");
 });
 
-app.get("/test-reminder", async (req, res) => {
-  await sendRemindersToAll();
-  res.send("Reminders sent!");
-});
-
 app.get("/webhook", (req, res) => {
   const mode = req.query["hub.mode"];
   const token = req.query["hub.verify_token"];
